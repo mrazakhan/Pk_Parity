ee24479b1a63416481875ed1404eb2a71408e556
mraza <mraza@uw.edu>
2016-03-28 00:27:09 -0700

    Added Rog

--- featgen/rog.py
+++ featgen/rog.py
@@ -3,22 +3,36 @@ import math
 import argparse
 import csv
 import sys
-# Read SF, Make A Copy, Transpose A and B
 def distance(l1_lat,l1_lng,l2_lat,l2_lng):
     R = 6371; # Radius of the earth in km
     d=0.0
-    try:	
+    try:
         l1_lat, l1_lng, l2_lat, l2_lng=float(l1_lat), float(l1_lng), float(l2_lat), float(l2_lng)
     except:
         l1_lat, l1_lng, l2_lat, l2_lng=0.0,0.0,0.0,0.0
-    dLat = (l1_lat-l2_lat)*math.pi/180  
-    dLon = (l1_lng-l2_lng)*math.pi/180 
+    dLat = (l1_lat-l2_lat)*math.pi/180
+    dLon = (l1_lng-l2_lng)*math.pi/180
     a = math.sin(dLat/2) * math.sin(dLat/2) +math.cos((l1_lat)*math.pi/180) * math.cos((l2_lat)*math.pi/180) * math.sin(dLon/2) * math.sin(dLon/2)
     c = 2 * math.atan2(math.sqrt(a), math.sqrt(1-a))
     d = R * c # Distance in km
     return d
 
-def geo_reach(input_file,loc_file, output_file, callerIdCol, receiverIdCol, callerCellCol, receiverCellCol):
+
+def calc_rog(df,keyCol,lat_col, lng_col, outputColumnName):
+    df[lat_col]=df[lat_col].apply(lambda x:float(x))
+    df[lng_col]=df[lng_col].apply(lambda x:float(x))
+    df_lat=df.groupby(keyCol,operations={'centroid_lat':gl.aggregate.AVG(lat_col)})
+    df_lng=df.groupby(keyCol,operations={'centroid_lng':gl.aggregate.AVG(lng_col)})
+    df_centroid=df_lat.join(df_lng, on=keyCol, how='inner')
+    df=df.join(df_centroid, on=keyCol, how='inner')
+    df['distance']=df.apply(lambda x:distance(x['centroid_lat'], x['centroid_lng'], x[lat_col], x[lng_col])**2.0)
+    df=df.groupby(keyCol,operations={outputColumnName:gl.aggregate.AVG('distance')})
+    df=df[[keyCol, outputColumnName]]
+    #df_out=df.join(self.df_sample, on='CallerId', how='right')
+    return df
+
+
+def rog(input_file,loc_file, output_file, callerIdCol, receiverIdCol, callerCellCol, receiverCellCol):
     #Index,TowerId,Lat,Lng,District
     loc_sf=gl.SFrame.read_csv(loc_file)
     sf1=gl.SFrame.read_csv(input_file)
@@ -44,16 +58,10 @@ def geo_reach(input_file,loc_file, output_file, callerIdCol, receiverIdCol, call
     loc_sf.rename({'TowerId':'LocA'}) 
     # Joining on the Caller Locations
     sf=sf.join(loc_sf, on='LocA', how='inner')
-    sf.rename({'Lat':'LatA','Lng':'LngA'})
-    # Joining on the Caller Location
-    loc_sf.rename({'LocA':'LocB'}) 
-    sf=sf.join(loc_sf, on='LocB', how='inner')
-    sf.rename({'Lat':'LatB','Lng':'LngB'})
-   
-    sf['dist']=sf.apply(lambda x:distance(x['LatA'],x['LngA'],x['LatB'],x['LngB']))
-    sf_final=sf.groupby(callerIdCol, {'AvgGeoReach':gl.aggregate.AVG('dist')})
+    
+    rog_sf=calc_rog(sf,callerIdCol,'Lat','Lng','rog')
      
-    sf_final.export_csv(output_file,quote_level=csv.QUOTE_NONE)
+    rog_sf.export_csv(output_file,quote_level=csv.QUOTE_NONE)
 
 
 if __name__=='__main__':
@@ -67,4 +75,4 @@ if __name__=='__main__':
     parser.add_argument('-rcc','--receiverCellCol',help='ReceiverCellCol', required=True)
 
     args=parser.parse_args()
-    geo_reach(args.input_file, args.loc_file,args.output_file, args.callerIdCol, args.receiverIdCol, args.callerCellCol, args.receiverCellCol)
+    rog(args.input_file, args.loc_file,args.output_file, args.callerIdCol, args.receiverIdCol, args.callerCellCol, args.receiverCellCol)
