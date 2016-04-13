efea4b6ed8e6256765eb9f09cf53a82bafa5c575
mraza <mraza@uw.edu>
2016-04-12 17:30:46 -0700

    Minor

--- featgen/run_district_level_features.sh
+++ featgen/run_district_level_features.sh
@@ -1,7 +1,4 @@
-#python district_level_features.py -if gsm_features.csv -of district_gsm_features.csv -ff ../data/UsersWithChangingDemographics.csv
 python district_level_features.py -if gsm_sms_features.csv -of district_gsm_sms_features.csv -ff ../data/UsersWithChangingDemographics.csv
 
 python district_level_features.py -if gsm_sms_features_filterCall_g2.csv -of district_gsm_sms_features_filterCall_g2.csv -ff ../data/UsersWithChangingDemographics.csv
-#python district_level_features.py -if bidir_gsm_features.csv -of district_bidir_gsm_features.csv -ff ../data/UsersWithChangingDemographics.csv
-#python district_level_features.py -if bidir_gsm_sms_features.csv -of district_bidir_gsm_sms_features.csv -ff ../data/UsersWithChangingDemographics.csv
 
